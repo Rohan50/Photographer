82b2eabdfc5a066f8b3f6fae3c077bdf9d7b8293
Rohan50 <pareekrohan50@gmail.com>
2026-03-10 15:41:07 +0530

    Change authorization method from Bearer to token

--- api/save-json.js
+++ api/save-json.js
@@ -19,12 +19,12 @@ export default async function handler(req, res) {
   try {
 
     const current = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28"
-      }
-    });
+  headers: {
+    Authorization: `token ${token}`,
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+  }
+});
 
     const data = await current.json();
 
@@ -34,14 +34,14 @@ export default async function handler(req, res) {
 
     const sha = data.sha;
 
-    const response = await fetch(url, {
-      method: "PUT",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28",
-        "Content-Type": "application/json"
-      },
+const response = await fetch(url, {
+  method: "PUT",
+  headers: {
+    Authorization: `token ${token}`,
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "Content-Type": "application/json"
+  },
       body: JSON.stringify({
         message: `Update ${fileName} from admin panel`,
         content: Buffer.from(JSON.stringify(content, null, 2)).toString("base64"),
